3d23f588525cd6ca2fd023113c5813691a659f8b
HS <HS@HSui-MacBook-Air.local>
2015-11-23 19:31:57 +0900

    header

---
+++ header.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <time.h>
+#include <utime.h>
+#include <string.h>
