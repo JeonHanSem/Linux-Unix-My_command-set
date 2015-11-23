0d8a68ce4612560fcc039a8772196bf52557b828
HS <HS@HSui-MacBook-Air.local>
2015-11-23 22:36:18 +0900

    chmod

---
+++ myChmod.c
@@ -0,0 +1,26 @@
+#include "header.h"
+
+#define MAXSIZE 100
+
+void Chmod(char *file,int mode){
+	if(chmod(file,mode)<0){ //입력받은 모드로 파일의 권한을 변경한다
+		perror("chmod error");
+		exit(1);
+	}
+}
+
+int main(int argc, char **argv){
+	int i;
+	int mode=strtol(argv[1],0,8); //모드를 8진수로 받는다
+
+	if(argc < 3){
+		printf("please input filename or mode number\n");
+		exit(1);
+	}
+	else{
+		for(i=2;i<argc;i++){
+			Chmod(argv[i],mode);
+		}
+	}
+	return 0;
+}
