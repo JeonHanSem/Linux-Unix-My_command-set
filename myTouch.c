f2f37f27e967e03b89ff5a4d6b8215d881fc27ae
HS <HS@HSui-MacBook-Air.local>
2015-11-29 16:16:07 +0900

    touch

--- myTouch.c
+++ myTouch.c
@@ -1,34 +1,26 @@
 #include "header.h"
 
 #define PERMS 0644
-void Touch(char *file){
+void fmake(char *file){
 	int fd;
 
-	fd = open(file,O_WRONLY | O_CREAT | O_APPEND, PERMS);
-	utime(file, NULL);
+	fd = open(file,O_WRONLY | O_CREAT, PERMS);
 	close(fd);
 }
 
-int fileCheck(char *file_name){
+void Touch(char *file_name){
 	struct stat finfo;	//파일의 정보를 담고있는 구조체
-	mode_t file_mode;
-	int i,flag=0;
 
-	//파일이 존재하지 않으면 생성해야하기때문에 flag=0을 반환한
+	//파일이 존재하지 않으면 생성한다
 	if ((stat(file_name, &finfo)) == -1){
-   		return flag;
+   		fmake(file_name);
    	}
-   	file_mode = finfo.st_mode;
 
-   	if (S_ISDIR(file_mode))	//디렉토리면 따로 처리
-		flag=2;
-	
-	return flag;
+	utime(file_name,NULL);
 }
 
 int main(int argc, char **argv){
-	char str;
-	int fd,i,f,flag;
+	int i;
 	//struct stat finfo;
 
 	if(argc < 2){
@@ -37,11 +29,7 @@ int main(int argc, char **argv){
 	}
 	else{
 		for(i=1;i<argc;i++){
-			flag=fileCheck(argv[i]);
-			if(flag==0)
-				Touch(argv[i]);
-			else if(flag==2)
-				utime(argv[i],NULL);
+			Touch(argv[i]);
 		}
 	}
 	
